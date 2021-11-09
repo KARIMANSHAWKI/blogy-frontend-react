2df5872cb1bac1fa29816d9943093f98a3862140
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-09 23:37:29 +0200

    adding login  page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import TopBar from './components/topBar/TopBar';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Settings from './pages/settings/Settings';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
 
@@ -8,7 +10,7 @@ function App() {
   return (
    <>
    <TopBar/>
-   <Write />
+   <Login />
    </>
   );
 }
