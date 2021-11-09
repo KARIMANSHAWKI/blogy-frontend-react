8d3ed7038d85b1e81c16dd6ac5af2cccb41ca74b
KARIMANSHAWKI <kariman0800@gmail.com>
2021-11-09 23:41:36 +0200

    adding register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import TopBar from './components/topBar/TopBar';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 import Settings from './pages/settings/Settings';
 import Single from './pages/single/Single';
 import Write from './pages/write/Write';
@@ -10,7 +11,7 @@ function App() {
   return (
    <>
    <TopBar/>
-   <Login />
+   <Register />
    </>
   );
 }
